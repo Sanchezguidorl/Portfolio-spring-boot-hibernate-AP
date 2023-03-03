7578b133b75aa8565ec0fdce42bd2a37c5cf2104
chenaoh <sanchezguidorl@gmail.com>
2023-03-03 12:35:57 -0300

    Se modifica directiva cors10

--- src/main/java/com/portfolio/GuidoSanchez/Security/CorsConfig.java
+++ src/main/java/com/portfolio/GuidoSanchez/Security/CorsConfig.java
@@ -1,24 +1,19 @@
 
 package com.portfolio.GuidoSanchez.Security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@EnableWebMvc
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer{
     
     
-    
-    @Bean
-   public WebMvcConfigurer corsConfigurer(){
-       
-       
-       return new WebMvcConfigurer(){
 
+    
            @Override
-           public void addCorsMappings(CorsRegistry registry){
+            public void addCorsMappings(CorsRegistry registry){
                registry.addMapping("/login")
                        .allowedOrigins("*")
                        .allowedMethods("*")
@@ -31,6 +26,4 @@ public class CorsConfig {
                        .allowedMethods("*");
                     
            }
-       };
-   }
 }
